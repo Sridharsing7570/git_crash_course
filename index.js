25a3ddd1f259bdb3938c8c50cd35d29c7cdb0767
Sridhar Sing <sridharsing14@gmail.com>
2024-10-05 17:12:29 +0530

    First comment

---
+++ index.js
@@ -0,0 +1,12 @@
+console.log("Node is running:");
+
+const age = 22;
+const name = "Sridhar Sing";
+
+const dob = "01/03/2002";
+
+function sayName() {
+    console.log("My name is " + name);
+}
+
+sayName();
